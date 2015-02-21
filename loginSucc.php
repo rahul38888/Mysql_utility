48af667451d6f9d2713db3abd77e7977b68c454c
Rahul <rahul38888@gmail.com>
2015-02-22 03:22:03 +0530

    Create loginSucc.php

---
+++ loginSucc.php
@@ -0,0 +1,16 @@
+<html>
+
+  <head>
+    <script>
+      window.onload = function(){
+        sleep(2000);
+        header('Location: loginErr.php');
+      }
+    </script>
+  </head>
+
+  <body>
+    Login was Succesfull. Wellcome.
+  </body>
+
+</html>
